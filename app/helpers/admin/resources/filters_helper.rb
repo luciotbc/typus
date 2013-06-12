be8b357136cee001bb4859e70625e173c293471d
Kevin Hoffman <khoffma4@gmail.com>
2013-06-12 09:57:41 -0500

    fixes call to get dropdown options

--- app/helpers/admin/resources/filters_helper.rb
+++ app/helpers/admin/resources/filters_helper.rb
@@ -14,7 +14,7 @@ module Admin::Resources::FiltersHelper
 
       locals[:scope_filters] = resource.get_typus_scope_filters.map do |key, value|
                                  { key: key,
-                                   value: set_context.send(filter.to_s.pluralize).to_a }
+                                   value: set_context.send(key.to_s.pluralize).to_a }
                                end
 
       render "helpers/admin/resources/filters", locals
