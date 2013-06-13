f3386462e90a08dc5ae24161659e741dfbd13902
Kevin Hoffman <khoffma4@gmail.com>
2013-06-13 16:36:16 -0500

    fixes typo

--- app/helpers/admin/resources/data_types/scope_filter_helper.rb
+++ app/helpers/admin/resources/data_types/scope_filter_helper.rb
@@ -2,7 +2,7 @@ module Admin::Resources::DataTypes::ScopeFilterHelper
 
   def scope_filter_selection(filter)
     items = [[Typus::I18n.t("Show by %{attribute}", :attribute => filter.to_s.underscore), ""]]
-    items += set_context.send(key.to_s.pluralize).to_a
+    items += set_context.send(filter.to_s.pluralize).to_a
   end
 
 end
