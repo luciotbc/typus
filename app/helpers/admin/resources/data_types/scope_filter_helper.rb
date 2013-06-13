2fa9b3bfd69110aa50fdefaed50c0185c9d86c13
Kevin Hoffman <khoffma4@gmail.com>
2013-06-13 16:46:38 -0500

    changes how the scope name is displayed in the dropdown

--- app/helpers/admin/resources/data_types/scope_filter_helper.rb
+++ app/helpers/admin/resources/data_types/scope_filter_helper.rb
@@ -1,7 +1,7 @@
 module Admin::Resources::DataTypes::ScopeFilterHelper
 
   def scope_filter_selection(filter)
-    items = [[Typus::I18n.t("Show by %{attribute}", :attribute => filter.to_s.underscore), ""]]
+    items = [[Typus::I18n.t("Show by %{attribute}", :attribute => filter.to_s.humanize.downcase), ""]]
     items += set_context.send(filter.to_s.pluralize).to_a
   end
 
