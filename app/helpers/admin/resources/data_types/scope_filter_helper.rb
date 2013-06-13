1993d38c266653b4f10fa7b0138a57d7c05f9e6b
Kevin Hoffman <khoffma4@gmail.com>
2013-06-13 16:29:34 -0500

    fixes calling underscore on a symbol

--- app/helpers/admin/resources/data_types/scope_filter_helper.rb
+++ app/helpers/admin/resources/data_types/scope_filter_helper.rb
@@ -1,7 +1,7 @@
 module Admin::Resources::DataTypes::ScopeFilterHelper
 
   def scope_filter_selection(filter)
-    items = [[Typus::I18n.t("Show by %{attribute}", :attribute => filter.underscore), ""]]
+    items = [[Typus::I18n.t("Show by %{attribute}", :attribute => filter.to_s.underscore), ""]]
     items += set_context.send(key.to_s.pluralize).to_a
   end
 
